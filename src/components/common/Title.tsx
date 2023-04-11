6d883993169910ee605714a6b02699b21663cd23
Isai Martinez <isai.martinez@yeico.com>
2023-04-11 10:40:56 -0600

    TextCenter

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -5,7 +5,7 @@ import { TitleProps } from 'interfaces/common'
 const Title = ({title}:TitleProps) => {
   return (
     <div className='flex items-center justify-center bg-no-repeat bg-cover bg-center h-40 w-72' style={{backgroundImage: `url(${TitleContainer})`}}>
-      <p className='text-white text-5xl'>{title}</p>
+      <p className='text-white text-5xl text-center	'>{title}</p>
     </div>
   )
 }
